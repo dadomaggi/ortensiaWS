47a22b79d70ad53cb5995d9a32c7fcfb6c27ca55
pi <davide.maggi83@gmail..com>
2017-12-19 00:16:28 +0100

    added median filter - II

--- DHT22/wlutils.py
+++ DHT22/wlutils.py
@@ -1,8 +1,26 @@
 #!/usr/bin/python
 
 import datetime
+import numpy as np
 
 def secs_from_midnight():
     now = datetime.datetime.now()
     midnight = datetime.datetime.combine(now.date(), datetime.time())
     return(now - midnight).seconds
+
+def medfilt (x, k):
+    """Apply a length-k median filter to a 1D array x.
+    Boundaries are extended by repeating endpoints.
+    """
+    assert k % 2 == 1, "Median filter length must be odd."
+    assert x.ndim == 1, "Input must be one-dimensional."
+    k2 = (k - 1) // 2
+    y = np.zeros ((len (x), k), dtype=x.dtype)
+    y[:,k2] = x
+    for i in range (k2):
+        j = k2 - i
+        y[j:,i] = x[:-j]
+        y[:j,i] = x[0]
+        y[:-j,-(i+1)] = x[j:]
+        y[-j:,-(i+1)] = x[-1]
+    return np.median (y, axis=1)
